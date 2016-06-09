68ddf5cd85b2a1bb6b5d9bdac710f0002f433553
S. Käppler <s.skr.k123@gmail.com>
2016-06-09 16:02:08 +0200

    Deine Taktik
    Die articlefunc.js ist vereinfacht, das css ist ein bisschen
    kaskadierender (für die articles, die noch kommen...)

--- codes/Praktikum/articlefunc.js
+++ codes/Praktikum/articlefunc.js
@@ -1,26 +1,16 @@
 function change_article(id){
 
-// bei einer Aktion zunächst alle ausblenden
-AlleAus()
+//get all articles
+var articles=document.getElementsByTagName("article");
 
-var article = document.getElementById(id);
-
-// Prüfung exixtiert eine Element mit dieser ID?
-if(!article)return true;
-
-  // Umschaltung
-  if(article.style.display == "none")
-  {
-  article.style.display = "block"
-  }
-  else
-  {
-  article.style.display = "none"
-  }
-
-return true;
+//iterate over articles, set display to block if ID matches desired article
+for (i=0; i<articles.length;i++){
+	if (articles[i].id==id){
+		articles[i].style.display = "block";
+	}else{
+		articles[i].style.display = "none";
+	}
 }
-function AlleAus() {
-   about_me.style.display = "none";
-   kompetenzen.style.display = "none";
+ 
+return true;
 }
